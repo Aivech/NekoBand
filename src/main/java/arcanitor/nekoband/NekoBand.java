2e70e778c2b470fe095448969953bfcfb50f469c
Arcanitor <damachinator@gmail.com>
2018-01-20 19:49:48 -0500

    Initial commit

---
+++ src/main/java/arcanitor/nekoband/NekoBand.java
@@ -0,0 +1,12 @@
+package arcanitor.nekoband;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = NekoBand.MODID,name = NekoBand.NAME,version = NekoBand.VERSION,useMetadata = true)
+public class NekoBand {
+    public static final String MODID = "nekoband";
+    public static final String NAME = "NekoBand";
+    public static final String VERSION = "0.0.1";
+
+
+}
