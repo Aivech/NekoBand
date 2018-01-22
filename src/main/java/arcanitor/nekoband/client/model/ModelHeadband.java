09254f8791be841d192b73d92be241753c18c499
Arcanitor <damachinator@gmail.com>
2018-01-22 09:46:30 -0500

    Fix the model

--- src/main/java/arcanitor/nekoband/client/model/ModelHeadband.java
+++ src/main/java/arcanitor/nekoband/client/model/ModelHeadband.java
@@ -12,37 +12,39 @@ public class ModelHeadband extends ModelArmorBase {
 
     public ModelHeadband() {
         this.textureHeight = 32;
-        this.textureWidth = 32;
+        this.textureWidth = 64;
+
+        float s = 0.01f;
 
         //width: px from center to left, offx: move origin to left x px
         //height: px down from bottom of head
         //depth: px backwards from center
-        this.headband = new ModelRenderer(this,0,-2);
+        this.headband = new ModelRenderer(this,0,0);
         this.headband.setRotationPoint(0.0F,0.0F,0.0F);
-        this.headband.addBox(-5,-1,-1,10,-8,2);
+        this.headband.addBox(-5,-9,-1,10,8,2,0.01f);
 
         this.earR = new ModelRenderer(this,0,0);
         this.earR.setRotationPoint(0.0F,0.0F,0.0F);
-        this.earR.addBox(-6,-10,0,3,2,1);//ear back
-        this.earR.addBox(-5,-11,0,1,1,1);//ear back top
-        this.earR.addBox(-6,-8,-1,1,1,1);//ear rim bottom
-        this.earR.addBox(-7,-10,-1,1,2,1);//ear rim outside
-        this.earR.addBox(-6,-11,-1,1,1,1);//ear rim outside tip
-        this.earR.addBox(-5,-12,-1,1,1,1);//ear rim tip
-        this.earR.addBox(-4,-11,-1,1,1,1);//ear rim inside tip
-        this.earR.addBox(-3,-10,-1,1,1,1);//ear rim inside
+        this.earR.addBox(-6,-10,-0.01f,3,2,1,s);//ear back
+        this.earR.addBox(-5,-11,-0.01f,1,1,1,s);//ear back top
+        this.earR.addBox(-6,-8,-1.01f,1,1,1,s);//ear rim bottom
+        this.earR.addBox(-7,-10,-1.01f,1,2,1,s);//ear rim outside
+        this.earR.addBox(-6,-11,-1.01f,1,1,1,s);//ear rim outside tip
+        this.earR.addBox(-5,-12,-1.01f,1,1,1,s);//ear rim tip
+        this.earR.addBox(-4,-11,-1.01f,1,1,1,s);//ear rim inside tip
+        this.earR.addBox(-3,-10,-1.01f,1,1,1,s);//ear rim inside
 
         this.earL = new ModelRenderer(this,0,0);
         this.earL.mirror = true;
-        this.earR.setRotationPoint(0.0F,0.0F,0.0F);
-        this.earR.addBox(6,-10,0,-3,2,1);//ear back
-        this.earR.addBox(5,-11,0,-1,1,1);//ear back top
-        this.earR.addBox(6,-8,-1,-1,1,1);//ear rim bottom
-        this.earR.addBox(7,-10,-1,-1,2,1);//ear rim outside
-        this.earR.addBox(6,-11,-1,-1,1,1);//ear rim outside tip
-        this.earR.addBox(5,-12,-1,-1,1,1);//ear rim tip
-        this.earR.addBox(4,-11,-1,-1,1,1);//ear rim inside tip
-        this.earR.addBox(3,-10,-1,-1,1,1);//ear rim inside
+        this.earL.setRotationPoint(0.0F,0.0F,0.0F);
+        this.earL.addBox(3,-10,-0.01f,3,2,1,s);//ear back
+        this.earL.addBox(4,-11,-0.01f,1,1,1,s);//ear back top
+        this.earL.addBox(5,-8,-1.01f,1,1,1,s);//ear rim bottom
+        this.earL.addBox(6,-10,-1.01f,1,2,1,s);//ear rim outside
+        this.earL.addBox(5,-11,-1.01f,1,1,1,s);//ear rim outside tip
+        this.earL.addBox(4,-12,-1.01f,1,1,1,s);//ear rim tip
+        this.earL.addBox(3,-11,-1.01f,1,1,1,s);//ear rim inside tip
+        this.earL.addBox(2,-10,-1.01f,1,1,1,s);//ear rim inside
 
 
         this.headband.addChild(this.earL);
